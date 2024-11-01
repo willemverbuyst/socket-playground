b4bcf51569a2c455f867da8243ac81cd951e7c26
Willem Verbuyst <willemverbuyst@gmail.com>
2024-11-01 05:41:39 +0100

    Serve html with deno server

--- deno-server/main.ts
+++ deno-server/main.ts
@@ -1,17 +1,46 @@
-Deno.serve((req) => {
-  if (req.headers.get("upgrade") != "websocket") {
-    return new Response(null, { status: 501 });
-  }
+// Deno.serve((req) => {
+//   if (req.headers.get("upgrade") != "websocket") {
+//     return new Response(null, { status: 501 });
+//   }
 
-  const { socket, response } = Deno.upgradeWebSocket(req);
+//   const { socket, response } = Deno.upgradeWebSocket(req);
 
-  socket.addEventListener("open", () => {
-    console.log("a client connected!");
-  });
+//   socket.addEventListener("open", () => {
+//     console.log("a client connected!");
+//   });
 
-  socket.addEventListener("close", () => {
-    console.log("Client disconnected");
-  });
+//   socket.addEventListener("close", () => {
+//     console.log("Client disconnected");
+//   });
 
-  return response;
+//   return response;
+// });
+
+Deno.serve({
+  port: 8004,
+  handler: async (request) => {
+    // If the request is a websocket upgrade,
+    // we need to use the Deno.upgradeWebSocket helper
+    if (request.headers.get("upgrade") === "websocket") {
+      const { socket, response } = Deno.upgradeWebSocket(request);
+
+      socket.onopen = () => {
+        console.log("CONNECTED");
+      };
+      socket.onmessage = (event) => {
+        console.log(`RECEIVED: ${event.data}`);
+        socket.send("pong");
+      };
+      socket.onclose = () => console.log("DISCONNECTED");
+      socket.onerror = (error) => console.error("ERROR:", error);
+
+      return response;
+    } else {
+      // If the request is a normal HTTP request,
+      // we serve the client HTML file.
+      const file = await Deno.open("./index.html", { read: true });
+
+      return new Response(file.readable);
+    }
+  },
 });
