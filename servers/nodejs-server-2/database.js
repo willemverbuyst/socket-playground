efc33bbaf9e4c0e9b10cbd7ccf401453401bb524
Willem Verbuyst <willemverbuyst@gmail.com>
2024-11-19 04:23:17 +0100

    Set up database

---
+++ servers/nodejs-server-2/database.js
@@ -0,0 +1,30 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "fugafoobardb",
+  database: "fugaFooBar",
+  password: "password",
+  port: 5432,
+});
+
+pool.query("DROP TABLE IF EXISTS data");
+
+pool
+  .query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+  .catch((err) => console.error("Error enabling uuid-ossp extension:", err));
+
+pool
+  .query(
+    `
+    CREATE TABLE IF NOT EXISTS data (
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        fuga TEXT,
+        foo TEXT,
+        bar INTEGER
+    )
+`
+  )
+  .catch((err) => console.error("Error creating table", err));
+
+module.exports = pool;
